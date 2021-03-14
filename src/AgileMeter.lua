a6cfe2e141fadf506b4ad3557db168f2b62edb07
octoplayer2 <54986649+octoplayer2@users.noreply.github.com>
2021-03-14 16:49:14 +0000

    Update AgileMeter.lua

--- src/AgileMeter.lua
+++ src/AgileMeter.lua
@@ -1,31 +1,39 @@
 -- AgileStatus Scene LUA code
---Provides Actions based on Octopus Agile daily electricity rates
+-- Provides Actions based on Octopus Agile daily electricity rates
 
 -- This module reads the prices published daily for the upcoming costs of electricity in 30 min slots.
 -- It extracts the Peak and Minimum charging times, and sets flags to indicate when the periods have started. 
---These can then be used to trigger scenes, e.g. to turn on car charging overnight, increase house temp prior to peak, and reduce it during peak hours.
+-- These can then be used to trigger scenes, e.g. to turn on car charging overnight, increase house temp prior to peak, and reduce it during peak hours.
 
--- Create a Device...
+--Create a Device...
 --  	Vera --> Apps  --> Develop Apps  --> Create Device
 --     	in "Upnp Device Filename" put D_PowerMeter1.xml
 --	in Device Name put (for example) AgileMeter
 --	Press Create Device.  After a restart the device will be created. Make a note of the DeviceId that Vera has assigned....
 --	Note at this stage the Variable list will be empty.
--- Create a Scene to hold this code
+
+--Create a Scene to hold this code
 -- 	Paste this code into a Vera Scene Lua section
 --	Edit the values below, put your DeviceId into the AgileDeviceId declaration...
 --	change the Product and Tariff to the appropriate ones for your account (See your Octopus Dashboard for the values)
 -- 	Press Submit on the Scene editor and ensure that Vera reloads. 
+--  After reload is complete, run the scene manually
 --	After 30s the main body willl be called for the first time, the variables will be created and the data fetched
---	To force an update of  the Variables, edit the ValidTilEpoch value to something small, eg delete last digit
--- While code is running:
+--	To force an update of the Variables, edit the ValidTilEpoch value to something small, eg delete last digit
+
+--To start the code automatically:
+--   To ensure it survives a restart, add the following line in Startup with the appropriate Scene No for your install, ensure that 
+-- luup.call_action(HAGWSID, "RunScene", {SceneNum = <SceneNo>}, 0) -- Run scene to start agile
+
+--While code is running:
 --	The code will call itself on the hour and half-hour (with a 5 sec delay to avoid any other actions at those times)
 --	If the prices have been updated, and the Peak slot has finished for the day, then the Price and new Start and End times will be updated
 --	The time will be checked against the Start and End times and a flag set to trigger actions or alerts:
 --	InPeak = 1 when Price is above a user-defined threshold
 --	InPrePeak = 1 for a user-defined period before peak rate starts
 --	InLowest = 1 when average price is lowest for a userdefined time, e.g. 4 hours to charge house batteries, or 1 hour for a washing machine
--- To use the flags:
+
+--To use the flags:
 --	If using AltUI, create a scene, add a trigger that is Watching one of the variables and add an appropriate action...
 --	eg Watch InPrePeak  when 	new == 1    run Action to turn up Electric Heating Thermostat by 2deg
 --	eg Watch InPeak, when   	new == 1    run Action to turn off EV Car Charging, and turn down Thermostat
@@ -39,32 +47,18 @@
 -- Version 1.1, Feb 2021 - Added Current and Next Price Variables, as suggested by Tony
 -- Version 1.2, Mar 2021 - Added Vat inc option. Corrected error in min for Epoch conversion. Force reload of values on reboot. 
 --                      - Ignore values > 24hrs ahead, modify peak detection to allow for restart during peak
+-- Version 1.3, Mar 2021 - Fixed bug in Vat inc option. Removed duplicate declaration. Added Pricing display string
 
 -- Code Start
 _G.GetAgile = GetAgile -- make function global so it can be re-called
     --Customise these values for your requirements
 local Product = "AGILE-18-02-21"
 local Tariff = "E-1R-AGILE-18-02-21-H"
-local PriceIncVAT = True
+local PriceIncVAT = true
 local PeakLevel = 21 -- above this level assumed to be in Peak rate
 local PeakAlert = 1 -- number of halfhours before peak
 local NCheapPeriods = 8 -- Cheapest period for N halfhours, eg 8 == 4 hours
 local AgileMeterId = TRV2["AgileMeter"] -- Insert Device ID of Agile Meter... Could make this autmatic one day
-    -------------------------------------------------------------
-
-function GetAgile()
-    local json = require("dkjson")
-    local tempfile = "/tmp/agile.json"
-    local EMSID = "urn:micasaverde-com:serviceId:EnergyMetering1"
-    ------------------------------------------------------------
-    --Customise these values for your requirements
-    local Product = "AGILE-18-02-21"
-    local Tariff = "E-1R-AGILE-18-02-21-H"
-    local PriceIncVAT = True
-    local PeakLevel = 25 -- above this level assumed to be in Peak rate
-    local PeakAlert = 1 -- number of halfhours before peak
-    local NCheapPeriods = 8 -- Cheapest period for N halfhours, eg 8 == 4 hours
-    local AgileMeterId = TRV2["AgileMeter"] -- Insert Device ID of Agile Meter... Could make this autmatic one day
     -------------------------------------------------------------
     -- PeakStart
     -- PeakEnd
@@ -73,18 +67,25 @@ function GetAgile()
     -- CurrentPrice
     -- NextPrice
 
+    -- Pricing - string format of pricing for ease of display
     -- Inpeak -> 1 when peak pricing applies
     -- PrePeakAlert -> 1 prior to peak
     -- InLowest -> 1 during min price window
     --
     -- ----------------------------------------------------------
+    -------------------------------------------------------------
+
+function GetAgile()
+    local json = require("dkjson")
+    local tempfile = "/tmp/agile.json"
+    local EMSID = "urn:micasaverde-com:serviceId:EnergyMetering1"
 
     local retData, js_res
     local price, LowestPrice = 0, math.huge
     local LowestPriceStart
     local PeakStart, PeakEnd, CheapStart = "", "H"
     local _NHrSum, _NHrMin
-    local PeakStartEpoch, PeakEndEpoch, CheapStartEpoch
+    local PeakStartEpoch, PeakEndEpoch, CheapStartEpoch, Now
 
     local SMC =
         'curl "https://api.octopus.energy/v1/products/' ..
@@ -109,26 +110,30 @@ function GetAgile()
 
             Now = os.time()
             local SlotsToGo = math.floor((ValidTilEpoch - Now) / 1800)   -- Get the number of 30 min slots between now and the latest value 
-            if PriceIncVat then
-                luup.variable_set(EMSID, "CurrentPrice", js_res.results[SlotsToGo + 1].value_inc_vat , AgileMeterId)
-                luup.variable_set(EMSID, "NextPrice", js_res.results[SlotsToGo].value_inc_vat , AgileMeterId)
+            if PriceIncVAT then
+                luup.variable_set(EMSID, "CurrentPrice", js_res.results[SlotsToGo + 1].value_inc_vat, AgileMeterId)
+                luup.variable_set(EMSID, "NextPrice", js_res.results[SlotsToGo].value_inc_vat, AgileMeterId)
             else
-                luup.variable_set(EMSID, "CurrentPrice", js_res.results[SlotsToGo + 1].value_exc_vat , AgileMeterId)
-                luup.variable_set(EMSID, "NextPrice", js_res.results[SlotsToGo].value_exc_vat , AgileMeterId)
+                luup.variable_set(EMSID, "CurrentPrice", js_res.results[SlotsToGo + 1].value_exc_vat, AgileMeterId)
+                luup.variable_set(EMSID, "NextPrice", js_res.results[SlotsToGo].value_exc_vat, AgileMeterId)
             end
-            if
-                (ValidTilEpoch > oldValidTil) and
+            luup.variable_set(EMSID, "Pricing", string.format("Price now=%.2f, next=%.2fp",luup.variable_get(EMSID, "CurrentPrice",AgileMeterId),
+                luup.variable_get(EMSID, "CurrentPrice",AgileMeterId)), AgileMeterId)
+            
+            if (ValidTilEpoch > oldValidTil) and
                     (Now > tonumber((luup.variable_get(EMSID, "PeakEndEpoch", AgileMeterId) or 0)))
-             then -- we have an update  ADD TEST FOR AFTER PEAK
+             then -- we have an update  and it is after today's peak
                 luup.log("Agile Data updated: " .. Year .. Month .. Day .. Hour .. Mins .. Secs)
                 luup.variable_set(EMSID, "ValidTil", Hour .. ":" .. Mins .. "/" .. Day, AgileMeterId)
 
                 _NHrMin = math.huge -- just a large value to start
                 i = math.max(1,SlotsToGo-47)  --Start with 24 hrs from Now, or first value
                 repeat
-                    if PriceIncVat then
+                    if PriceIncVAT then
+               luup.log("Agile Price INC vat: ")
                         price = js_res.results[i].value_inc_vat
                     else
+               luup.log("Agile Price EXC vat: ")
                         price = js_res.results[i].value_exc_vat
                     end
                     luup.log("Agile price from = " .. js_res.results[i].valid_from .. ": " .. price)
